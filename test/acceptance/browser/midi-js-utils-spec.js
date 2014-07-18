0011737a21ec6fff791dd8458faf055056bfd300
Jason Storey <jason.storey@unrulymedia.com>
2014-07-18 12:09:43 +0100

    Delete old spec (renamed to load-test)

--- test/acceptance/browser/midi-js-utils-spec.js
+++
@@ -1,6 +0,0 @@
-var midiJsUtils = require('../../../src/midi-js-utils.js');
-
-midiJsUtils.load('http://localhost:8000/test/resources/zelda.mid', function(err, midi) {
-	console.log(err);
-	console.log(midi);
-});
